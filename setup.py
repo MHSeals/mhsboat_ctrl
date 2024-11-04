f5a21c065b2dd74fb28cac54c1ef44f24315e0dd
Alec Jensen <59067840+alec-jensen@users.noreply.github.com>
2024-11-04 15:06:18 -0600

    feat: add boat description URDF and launch configuration; update package dependencies and RViz settings

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'description'), glob(os.path.join('description', '*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*'))),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
