7dfe0e2599e32e9e8ead2aeb32653daef750ff17
1unarzDev <lunarzDev@outlook.com>
2025-02-28 13:37:40 -0600

    Recorder

--- mhsboat_ctrl/bag_recorder.py
+++ mhsboat_ctrl/bag_recorder.py
@@ -1,17 +1,22 @@
-# TODO: Test the functionality of this script and fix errors
-
-""" 
-NOTE: I think the most likely issue is going to have to do with message serialization
-      since we're using a custom message. We also need to add a subscription to cmd_vel.
-"""
+#!/usr/bin/env python3
 
 import rclpy
 from rclpy.node import Node
 from rclpy.serialization import serialize_message
+
+from sensor_msgs.msg import Image
 from geometry_msgs.msg import TwistStamped
 from boat_interfaces.msg import BuoyMap
 
 import rosbag2_py
+
+import datetime
+import os
+
+parent_dir = "bag_files"  
+os.makedirs(parent_dir, exist_ok=True)
+
+bag_dir = os.path.join(parent_dir, f'bag_{datetime.datetime.now().strftime("%Y%m%d_%H%M%S")}')
 
 class BagRecorder(Node):
     def __init__(self):
@@ -19,7 +24,7 @@ class BagRecorder(Node):
         self.writer = rosbag2_py.SequentialWriter()
 
         storage_options = rosbag2_py.StorageOptions(
-            uri='bag_files', # Location of the bag directory
+            uri=bag_dir, # Location of the bag directory
             storage_id='sqlite3')
         converter_options = rosbag2_py.ConverterOptions('', '')
         self.writer.open(storage_options, converter_options)
@@ -31,13 +36,12 @@ class BagRecorder(Node):
         self.writer.create_topic(topic_info)
 
         self.map_subscription = self.create_subscription(
-            BuoyMap,
+            Clusters,
             '/mhsboat_ctrl/map',
-            self.map_callback,
+            self.cluster_callback,
             10)
-        self.subscription
 
-    def map_callback(self, msg):
+    def cluster_callback(self, msg):
         self.writer.write(
             '/mhsboat_ctrl/map',
             serialize_message(msg),
